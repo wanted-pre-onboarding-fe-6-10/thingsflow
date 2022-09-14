b5416f3bd87a94960340f6e1964f85ee4ff523f5
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-09-14 21:50:55 +0900

    Feat: Detail 페이지 api 호출

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -2,6 +2,8 @@ import { getIssueList } from 'api/api';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { IssueType } from 'src/type/type';
+import styled from 'styled-components';
+import dateConvert from 'utils/convertStringTodate';
 
 const Detail = () => {
   const { number } = useParams();
@@ -17,16 +19,40 @@ const Detail = () => {
   }, []);
 
   return (
-    <div>
-      <>{issueDetail?.user.login}</>
-      <>{issueDetail?.created_at}</>
-      <>{issueDetail?.number}</>
-      <>{issueDetail?.title}</>
-      <>{issueDetail?.comments}</>
-      <>{issueDetail?.user.avatar_url}</>
+    <Container>
+      <Box>
+        <ProfileImg src={issueDetail?.user.avatar_url} />
+        <IssueInfo>
+          <Wrapper>
+            <TitleBox>
+              <Title>#{issueDetail?.number} </Title>
+              <Title> {issueDetail?.title}</Title>
+            </TitleBox>
+            <SubInfo>
+              <InfoText>작성자: {issueDetail?.user.login} </InfoText>
+              <>작성일: {dateConvert(issueDetail?.created_at as Date)}</>
+            </SubInfo>
+          </Wrapper>
+          <Comments>{issueDetail?.comments}</Comments>
+        </IssueInfo>
+      </Box>
       <>{issueDetail?.body}</>
-    </div>
+    </Container>
   );
 };
 
 export default Detail;
+
+const Container = styled.div``;
+const Box = styled.div``;
+const ProfileImg = styled.img`
+  width: 15%;
+  border-radius: 50%;
+`;
+const IssueInfo = styled.div``;
+const TitleBox = styled.p``;
+const Title = styled.span``;
+const InfoText = styled.span``;
+const SubInfo = styled.p``;
+const Wrapper = styled.div``;
+const Comments = styled.p``;
