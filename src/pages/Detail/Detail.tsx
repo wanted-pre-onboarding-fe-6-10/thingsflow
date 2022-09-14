ab9e0b194ab6503b965e58e00da1f94bdc5f9f5f
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-09-14 19:25:39 +0900

    Feat: issue detail api 호출

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,5 +1,32 @@
+import { getIssueList } from 'api/api';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { IssueType } from 'src/type/type';
+
 const Detail = () => {
-  return <>Detail</>;
+  const { number } = useParams();
+  const issueURL = `/issues/`;
+  const [issueDetail, setIssueDetail] = useState<IssueType | undefined>();
+  const getIssueDetail = async () => {
+    const response = await getIssueList(issueURL + number);
+    setIssueDetail(response);
+  };
+
+  useEffect(() => {
+    getIssueDetail();
+  }, []);
+
+  return (
+    <div>
+      <>{issueDetail?.user.login}</>
+      <>{issueDetail?.created_at}</>
+      <>{issueDetail?.number}</>
+      <>{issueDetail?.title}</>
+      <>{issueDetail?.comments}</>
+      <>{issueDetail?.user.avatar_url}</>
+      <>{issueDetail?.body}</>
+    </div>
+  );
 };
 
 export default Detail;
