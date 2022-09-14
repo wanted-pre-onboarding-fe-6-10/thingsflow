de91e55e2b9feea5851d4bc3fd6554a0ac29b4b4
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-09-14 21:47:47 +0900

    Feat: context api Provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import IssueProvider from 'pages/Issue/IssueProvider';
+import Router from './Router';
+import styled from 'styled-components';
+
 function App() {
-  return <>App.tsx</>;
+  return (
+    <IssueProvider>
+      <Header>Angular/Angular-cli</Header>
+      <Router />
+    </IssueProvider>
+  );
 }
 
 export default App;
+
+const Header = styled.h1``;
