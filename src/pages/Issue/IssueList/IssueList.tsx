15869b607e4bc7e01c4362367bcc59a06a77b017
eazae <xynezae@gmail.com>
2022-09-14 22:58:09 +0900

    Clean: unused files and imports

--- src/pages/Issue/IssueList/IssueList.tsx
+++ src/pages/Issue/IssueList/IssueList.tsx
@@ -1,55 +1,55 @@
 import Detail from 'pages/Detail/Detail';
-import { useState } from 'react';
-import { useCallback } from 'react';
-import { useRef } from 'react';
-import useIssueSearch from 'hooks/useIssueSearch';
-
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import IssueItem from './IssueItem';
 import { useIssue } from 'context/IssueContext';
 import Spinner from 'components/Spinner';
-import { useEffect } from 'react';
+import { IssueType } from 'src/types/IssueType';
 
 const IssueList = () => {
-  const [pageNumber, setPageNumber] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [pageNumber, setPageNumber] = useState(1);
+  // const [isLoading, setIsLoading] = useState(false);
 
   //TODO useContext()로 대체
-  const { issuess } = useIssue();
+  const { issues, isLoading, error, setTargetRepository, lastIssueElementRef } = useIssue();
 
-  const [focusedIssue, setFocusedIssue] = useState();
-
-  const { issues, hasMore, loading, error } = useIssueSearch({ query: '', pageNumber });
-
-  const observer = useRef<any>();
-
-  const lastIssueElementRef = useCallback(
-    (node: any) => {
-      if (loading) return;
-      if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver(entries => {
-        if (entries[0].isIntersecting && hasMore) {
-          setPageNumber(prev => prev + 1);
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [loading, hasMore]
-  );
+  const [focusedIssue, setFocusedIssue] = useState(-1);
 
   useEffect(() => {
-    // TODO Delete: 시연용
-    setInterval(() => console.log(''), 10000);
+    setTargetRepository('angular/angular-cli');
+  }, []);
 
-    setIsLoading(loading);
-  }, [loading]);
+  // const { issues, hasMore, loading, error } = useIssueSearch({ query: '', pageNumber });
+
+  // const observer = useRef<any>();
+
+  // const lastIssueElementRef = useCallback(
+  //   (node: any) => {
+  //     if (loading) return;
+  //     if (observer.current) observer.current.disconnect();
+  //     observer.current = new IntersectionObserver(entries => {
+  //       if (entries[0].isIntersecting && hasMore) {
+  //         setPageNumber(prev => prev + 1);
+  //       }
+  //     });
+  //     if (node) observer.current.observe(node);
+  //   },
+  //   [loading, hasMore]
+  // );
+
+  // useEffect(() => {
+  //   // TODO Delete: 시연용
+  //   setInterval(() => console.log(''), 10000);
+
+  //   setIsLoading(loading);
+  // }, [loading]);
 
   return (
     <Box>
       <ListWrapper>
-        {issues.map((item, idx) => (
+        {issues.map((item: IssueType, idx: number) => (
           <IssueItemWrapper key={idx}>
-            <IssueItem issue={item} setFocusedIssue={setFocusedIssue} />
+            <IssueItem issue={item} focusedIssue={focusedIssue} setFocusedIssue={setFocusedIssue} />
             {issues.length === idx + 1 ? <div ref={lastIssueElementRef} /> : null}
             {idx === 4 ? (
               <Banner href="https://thingsflow.com/ko/home">
@@ -61,11 +61,11 @@ const IssueList = () => {
         {isLoading && <Spinner />}
       </ListWrapper>
 
-      {focusedIssue && (
+      {/* {focusedIssue !== -1 && (
         <DetailWrapper>
           <Detail issueNumber={focusedIssue} />
         </DetailWrapper>
-      )}
+      )} */}
     </Box>
   );
 };
@@ -74,7 +74,9 @@ const Box = styled.div`
   display: grid;
   width: 100%;
   grid-template-columns: 1fr 1fr;
+  column-gap: 1rem;
 
+  overflow-y: scroll;
   @media screen and (max-width: 620px) {
     grid-template-columns: 1fr;
   }
@@ -83,11 +85,20 @@ const Box = styled.div`
 const ListWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  /* align-items: center; */
+  align-items: center;
 `;
-const IssueItemWrapper = styled.div``;
 
-const DetailWrapper = styled.div``;
+const IssueItemWrapper = styled.div`
+  width: 100%;
+  & + & {
+    margin-top: 0.5rem;
+  }
+`;
+
+const DetailWrapper = styled.div`
+  background-color: white;
+  padding: 2rem;
+`;
 
 const Banner = styled.a``;
 
@@ -97,6 +108,7 @@ const BannerImg = styled.img`
   max-height: 100px;
   opacity: 0.5;
   background-color: ${props => props.theme.subBgColor};
+  margin-top: 1rem;
 `;
 
 export default IssueList;
