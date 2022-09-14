c17fc3baaa86c4b8b888ebc5209db3f25e787284
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-09-14 22:29:52 +0900

    Design: Detail page UI Design

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -2,6 +2,7 @@ import { getIssueList } from 'api/api';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { IssueType } from 'src/type/type';
+import ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
 import dateConvert from 'utils/convertStringTodate';
 
@@ -36,23 +37,61 @@ const Detail = () => {
           <Comments>{issueDetail?.comments}</Comments>
         </IssueInfo>
       </Box>
-      <>{issueDetail?.body}</>
+      <MainBox>
+        <ReactMarkdown>{issueDetail?.body as string}</ReactMarkdown>
+      </MainBox>
     </Container>
   );
 };
 
 export default Detail;
 
-const Container = styled.div``;
-const Box = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const MainBox = styled.div`
+  width: 70%;
+  margin: 2rem;
+  line-height: 2rem;
+`;
+const Box = styled.div`
+  width: 70%;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #707070;
+`;
 const ProfileImg = styled.img`
   width: 15%;
   border-radius: 50%;
+  margin: 2rem;
 `;
-const IssueInfo = styled.div``;
-const TitleBox = styled.p``;
-const Title = styled.span``;
-const InfoText = styled.span``;
-const SubInfo = styled.p``;
-const Wrapper = styled.div``;
-const Comments = styled.p``;
+const IssueInfo = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 2rem;
+  width: 70%;
+`;
+const Wrapper = styled.div`
+  width: 80%;
+`;
+const TitleBox = styled.p`
+  font-weight: bold;
+  font-size: 1rem;
+`;
+const Title = styled.span`
+  margin-right: 1rem;
+`;
+const InfoText = styled.span`
+  margin-right: 1rem;
+`;
+const SubInfo = styled.p`
+  font-size: 0.8rem;
+`;
+
+const Comments = styled.p`
+  width: 10%;
+  margin: 0 2rem;
+`;
