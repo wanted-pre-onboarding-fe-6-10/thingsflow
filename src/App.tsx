25736fae4e7a4b4de3b56cb3f7004bf315551a49
cos4338@gmail.como <seungyeonchoo@chuseung-yeon-ui-MacBookAir.local>
2022-09-14 22:29:18 +0900

    Feat: create and design Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import IssueProvider from 'pages/Issue/IssueProvider';
 import Router from './Router';
-import styled from 'styled-components';
 
 function App() {
   return (
     <IssueProvider>
-      <Header>Angular/Angular-cli</Header>
       <Router />
     </IssueProvider>
   );
 }
 
 export default App;
-
-const Header = styled.h1``;
