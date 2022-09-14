5e8c3b1d427811d079ee5bfd8dd261bed4b9c18b
Dev-jwJeong <rgn95652@gmail.com>
2022-09-15 00:27:09 +0900

    Feat : markdown 변환 라이브러리 설정

--- src/pages/Detail/MarkdownRender.tsx
+++ src/pages/Detail/MarkdownRender.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
+import remarkGfm from 'remark-gfm';
 
 type Markdown = {
   markdown: string | undefined;
 };
 
 const MarkdownRender = ({ markdown }: Markdown) => {
-  return <Container>{markdown && <ReactMarkdown>{markdown}</ReactMarkdown>}</Container>;
+  return (
+    <Container>
+      {markdown && <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>}
+    </Container>
+  );
 };
 
 export default MarkdownRender;
